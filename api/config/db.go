c781a47bcba865f863888e0fb56446e78776859a
Nathan McAlinden <nathan.mcalinden@unosquare.com>
2022-06-14 12:27:24 +0100

    - Add unversioned files

---
+++ api/config/db.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"os"
+	"time"
+)
+
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "password"
+	dbname   = "footpaldb"
+)
+
+var conn *sqlx.DB
+
+func InitializeDatabase() {
+	db, err := sqlx.Open("postgres", getDSN())
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// https://github.com/go-sql-driver/mysql#important-settings
+	db.SetConnMaxLifetime(time.Hour)
+	db.SetConnMaxIdleTime(time.Hour)
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+
+	conn = db
+}
+
+func GetConnection() *sqlx.DB {
+	return conn
+}
+
+func getDSN() string {
+
+	dbConn := os.Getenv("API_DB_URL")
+	if dbConn != "" {
+		return dbConn
+	}
+	return fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+}
---
+++ api/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/helmet/v2"
+	"github.com/nmcalinden/footpal/config"
+	"github.com/nmcalinden/footpal/routers"
+	"github.com/nmcalinden/footpal/utils"
+)
+
+// @title Footpal API
+// @version 1.0
+// @description This is swagger for Footpal
+// @termsOfService http://swagger.io/terms/
+// @contact.name Footpal Support
+// @contact.email nathan.mcalinden@unosquare.com
+// @host 127.0.0.1:8081
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /
+func main() {
+
+	fiberConfig := fiber.Config{
+		ErrorHandler: defaultErrorHandler,
+	}
+	app := fiber.New(fiberConfig)
+
+	app.Use(cors.New())
+	app.Use(helmet.New())
+	app.Use(logger.New())
+	app.Use(recover.New(recover.Config{EnableStackTrace: true}))
+
+	config.InitializeDatabase()
+
+	utils.ConfigureSwagger(app)
+
+	routers.ConfigureHealthHandler(app)
+	routers.ConfigureUserHandlers(app)
+	routers.ConfigureVenueHandlers(app)
+	routers.ConfigurePlayerHandlers(app)
+	routers.ConfigureSquadPlayers(app)
+	routers.ConfigureBookingHandlers(app)
+	routers.ConfigureMatchHandlers(app)
+
+	log.Fatal(app.Listen(":8081"))
+}
+
+func defaultErrorHandler(ctx *fiber.Ctx, err error) error {
+	code := fiber.StatusInternalServerError
+	message := err.Error()
+
+	if e, ok := err.(*fiber.Error); ok {
+		code = e.Code
+	}
+
+	errors := map[string]string{"reason": message}
+	return utils.BuildMultipleErrorResponse(ctx, code, "Something has gone wrong", errors)
+}
